98781c499d14047d6fa185b6f42d2cb4c3300eb5
jwtrueb <jwtrueb@gmail.com>
2014-03-19 21:31:40 -0500

    Updated with that on server

--- Clicker Page/WebContent/logAns.php
+++ Clicker Page/WebContent/logAns.php
@@ -3,31 +3,35 @@
 	$oldVotes = file_get_contents("answers.txt");
 	$f = fopen("answers.txt", "w+");
 
-	$answers = "0 0 0 0 1"; //$_POST["answers"];
+	$answer = $_GET['answer'];
 	$oldAbcd = explode(" ", $oldVotes); //the old ones 
-	$newAbcd = explode(" ", $answers); //the new ones 
-
-    print_r($newAbcd);
-	//check to see if any of the new votes went up or if they were reset
-	$zeros = True;
-	for($i=0; $i<5; $i++){
-		if($newAbcd[$i] !=0){
-			$zeros = False;
-		}
+	echo $answer;
+        
+	if("a" == $answer){
+		$oldAbcd[0] += 1;
 	}
-
-	if($zeros){
-		$oldAbcd = $newAbcd;
+	else if("b" == $answer){
+		$oldAbcd[1] += 1;
+	}
+	else if("c" == $answer){
+		$oldAbcd[2] += 1;
+	}	
+	else if("d" == $answer){
+		$oldAbcd[3] += 1;
+	}
+	else if("e" == $answer){
+		$oldAbcd[4] += 1;
 	}
 	else{
-		for($i=0; $i < 5; $i++) {
-			if($newAbcd[$i] > 0) {
-				$oldAbcd[$i] += 1;
-			}
-		}
+		$oldAbcd[0] = 0;
+		$oldAbcd[1] = 0;
+		$oldAbcd[2] = 0;
+		$oldAbcd[3] = 0;
+		$oldAbcd[4] = 0;
 	}
 	
-	$answers = implode(" ", $oldAbcd);
-	fwrite($f, $answers);
+	$string = "{$oldAbcd[0]} {$oldAbcd[1]} {$oldAbcd[2]} {$oldAbcd[3]} {$oldAbcd[4]}"; 
+	fwrite($f, $string);
+        //fwrite($f, "10 8 7 3 4");
 	fclose($f);
 ?>
